8c4e14070023d791e91c097a342402ff93a42561
LiliBruijn <lilibruijn_7@hotmail.com>
2023-05-16 14:17:12 +0200

    Whole project

---
+++ Source/KerkCroissant/KerkCroissantGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KerkCroissantGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKerkCroissantGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKerkCroissantGameMode();
+};
+
+
+
